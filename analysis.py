6d98c4ee540290d77cb1321a8e1ff1049788aea3
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-12-12 23:53:16 +0200

    Add disk_space_analyser function

--- analysis.py
+++ analysis.py
@@ -1,12 +1,12 @@
 # Functions:
-# File Count Analyzer: Finds the folder within a directory tree that contains the highest number of files.
-# Disk Space Analyzer: Identifies the folder that occupies the most disk space.
-# File Type Distribution Analyzer: Analyzes the distribution of different file types (like .txt, .jpg, .py, etc.) within a directory and its subdirectories.
+# File Count Analyser: Finds the folder within a directory tree that contains the highest number of files.
+# Disk Space Analyser: Identifies the folder that occupies the most disk space.
+# File Type Distribution Analyzer: Analyses the distribution of different file types (like .txt, .jpg, .py, etc.) within a directory and its subdirectories.
 # Large File Finder: Identifies the largest files within a directory tree.
 
 import os
 from pathlib import Path
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 def file_count_analyser(directory):
@@ -30,8 +30,29 @@ def file_count_analyser(directory):
     return most_common[0] if most_common else (None, 0)
 
 
-def disk_space_analyser():
-    pass
+def disk_space_analyser(directory):
+    """
+        Analyzes the given directory to find the folder that occupies the most disk space.
+
+        Params:
+        directory (str): Path to the directory to analyze.
+
+        Returns:
+        tuple: A tuple containing the path to the folder occupying the most disk space and the total size in bytes.
+    """
+    base_path = Path(directory)
+    space_usage = defaultdict(int)
+
+    for file in base_path.rglob('*'):
+        if file.is_file():
+            space_usage[file.parent] += file.stat().st_size
+
+    if not space_usage:
+        return None, 0
+
+    max_space_folder = max(space_usage, key=space_usage.get)
+    max_space = space_usage[max_space_folder]
+    return max_space_folder, max_space
 
 
 def file_type_dist_analyser():
@@ -44,12 +65,15 @@ def large_type_finder():
 
 def main():
     user_choice = input('Select function:'
-                        '\n1. File Count Analyzer'
+                        '\n1. File Count Analyser'
+                        '\n2. Disk Space Analyser'
+                        '\n3. File Type Distribution Analyser'
+                        '\n4. Large File Finder'
                         '\n5. Exit'
                         '\n> ')
     while True:
         if user_choice == '1':
-            directory_path = input("Enter the path to the directory to analyze: ")
+            directory_path = input("Enter the path to the directory to analyse: ")
             folder, count = file_count_analyser(directory_path)
             if folder:
                 print(f'The folder with the most files is: {folder} with {count} files.')
@@ -58,6 +82,16 @@ def main():
                 break
             else:
                 print("No files found in the given directory.")
+        elif user_choice == '2':
+            directory_path = input("Enter the path to the directory to analyse: ")
+            folder, space = disk_space_analyser(directory_path)
+            if folder:
+                print(f'The folder occupying the most disk space is: {folder} using {space} bytes.')
+            elif directory_path == 'exit':
+                print("Exiting.")
+                break
+            else:
+                print("No files found in the given directory or directory is empty.")
         elif user_choice == '5':
             print("Exiting.")
             break
