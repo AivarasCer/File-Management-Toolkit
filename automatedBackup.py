380dfce031d8eadaf0f02bd16f07f1333a4748bb
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-12-15 18:01:29 +0200

    Add skeleton for automatedBackup.py

---
+++ automatedBackup.py
@@ -0,0 +1,26 @@
+# Automated Backup Script
+# Program that automatically backs up specified directories to a local drive after each addition/edit
+
+from watchdog.events import FileSystemEventHandler
+
+
+class BackupHandler(FileSystemEventHandler):
+
+    def __init__(self, source_dir, backup_dir):
+        self.source_dir = source_dir
+        self.backup_dir = backup_dir
+
+    def on_modified(self, event):
+        pass
+
+    def on_created(self, event):
+        pass
+
+
+def start_backup(monitor_dir, backup_dir):
+    pass
+
+if __name__ == "__main__":
+    monitor = input('Enter the path to the directory to monitor: ')
+    backup = input('Enter the path to the backup directory: ')
+    start_backup(monitor, backup)
