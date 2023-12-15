206e2053d35a84f124e9a31edfb54e1acdc9b635
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-12-15 18:16:38 +0200

    Add Path and other small updates

--- automatedBackup.py
+++ automatedBackup.py
@@ -1,19 +1,23 @@
 # Automated Backup Script
 # Program that automatically backs up specified directories to a local drive after each addition/edit
 
+from pathlib import Path
 from watchdog.events import FileSystemEventHandler
 
 
 class BackupHandler(FileSystemEventHandler):
 
     def __init__(self, source_dir, backup_dir):
-        self.source_dir = source_dir
-        self.backup_dir = backup_dir
+        self.source_dir = Path(source_dir)
+        self.backup_dir = Path(backup_dir)
 
     def on_modified(self, event):
         pass
 
     def on_created(self, event):
+        pass
+
+    def backup_file(self, path):
         pass
 
 
