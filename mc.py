1c393d7898bfb6106c294496ea3dfaf416f00e0c
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-12-17 22:41:13 +0200

    Add save and load functions

--- mc.py
+++ mc.py
@@ -6,15 +6,42 @@
 # py mc.py load <keyword> - to list keywords
 
 import shelve
+import pyperclip
 import sys
 
 
 class MultiClip:
-    pass
+
+    def __init__(self, db_path='clipboard_items.db'):
+        self.db_path = db_path
+
+    def save_to_cliboard(self, key):
+        with shelve.open(self.db_path) as storage:
+            storage[key] = pyperclip.paste()
+            print(f"Text saved under key '{key}'.")
+
+    def load_from_clipboard(self, key):
+        with shelve.open(self.db_path) as storage:
+            text = storage.get(key, None)
+            if text:
+                pyperclip.copy(text)
+                print(f"Text under key '{key}' copied to clipboard.")
+            else:
+                print(f"No text found for key '{key}'.")
+
+    def list_clipboard_keys(self):
+        pass
 
 
 def main():
-    pass
+    clipboarder = MultiClip()
+
+    if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+        clipboarder.save_to_cliboard(sys.argv[2])
+
+    elif len(sys.argv) == 3 and sys.argv[1].lower() == 'load':
+        clipboarder.load_from_clipboard(sys.argv[2])
+
 
 if __name__ == "__main__":
     main()
