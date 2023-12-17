9b3bdba34ea14d57a3e0813ee3851b70b7fa961d
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-12-17 18:25:38 +0200

    Add metadataEditor.py skeleton

---
+++ metadataEditor.py
@@ -0,0 +1,26 @@
+# Metadata Editor
+# A script that can edit or remove metadata from jpg or pdf files.
+
+from pathlib import Path
+
+
+class MetadataEditor:
+
+    def __init__(self, file_path):
+        self.file_path = Path(file_path)
+
+    def view_efix(self):
+        pass
+
+    def edit_exif(self):
+        pass
+
+    def remove_exif(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
