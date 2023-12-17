e128b2a765e3962490d31120b15d3ba0fe6e63d8
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-12-17 22:14:48 +0200

    Add mc.py skeleton

---
+++ mc.py
@@ -0,0 +1,20 @@
+# Multi Clipboard
+# This program saves each piece of clipboard text under a keyword.
+# This text can later be loaded to the clipboard again.
+# py mc.py save <keyword> - to save
+# py mc.py load <keyword> - to load
+# py mc.py load <keyword> - to list keywords
+
+import shelve
+import sys
+
+
+class MultiClip:
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
