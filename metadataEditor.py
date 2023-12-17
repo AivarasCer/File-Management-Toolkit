02a0cf3a4b0af7f84f6b37ad920006e3afee0aa4
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-12-17 19:23:33 +0200

    Add metadataEditor.py view_exif and remove_exif functionality

--- metadataEditor.py
+++ metadataEditor.py
@@ -1,7 +1,10 @@
 # Metadata Editor
-# A script that can edit or remove metadata from jpg or pdf files.
+# A script that can view or remove exif data from jpg files.
 
+import piexif
 from pathlib import Path
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 
 class MetadataEditor:
@@ -9,18 +12,59 @@ class MetadataEditor:
     def __init__(self, file_path):
         self.file_path = Path(file_path)
 
-    def view_efix(self):
-        pass
+    def view_exif(self):
+        if self.file_path.suffix.lower() in ['.jpg', '.jpeg']:
+            with Image.open(self.file_path) as img:
+                exif_data = img.info.get('exif')
+                if not exif_data:
+                    print('No EXIF data found.')
+                    return
 
-    def edit_exif(self):
-        pass
+                try:
+                    exif_dict = piexif.load(exif_data)
+                    for ifd in exif_dict:
+                        print(f"IFD: {ifd}")
+                        if exif_dict[ifd]:
+                            for tag in exif_dict[ifd]:
+                                tag_name = TAGS.get(tag, tag)
+                                value = exif_dict[ifd][tag]
+                                print(f"  {tag_name}: {value}")
+                    else:
+                        print(f"  No data found in {ifd}")
+
+                except Exception as e:
+                    print(f"Error reading EXIF data: {e}")
+        else:
+            print('File format not supported for EXIF viewing.')
 
     def remove_exif(self):
-        pass
+        if self.file_path.suffix.lower() in ['.jpg', '.jpeg']:
+            piexif.remove(self.file_path.as_posix())
+            print(f'Removed EXIF data from {self.file_path}')
+        else:
+            print('File format not supported for EXIF removal.')
 
 
 def main():
-    pass
+    file_path = input('Enter the path of the JPEG file: ')
+    editor = MetadataEditor(file_path)
+
+    while True:
+        user_action = int(input('Choose action:'
+                                '\n1. View EXIF data'
+                                '\n2. Remove EXIF data'
+                                '\n3. Exit'
+                                '\n> '))
+
+        if user_action == 1:
+            editor.view_exif()
+
+        elif user_action == 2:
+            editor.remove_exif()
+
+        elif user_action == 3:
+            print('Exiting.')
+            break
 
 if __name__ == "__main__":
     main()
