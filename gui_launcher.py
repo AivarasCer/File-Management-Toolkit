e0da7b4528bcda64b9d116af8ab6806d6e036e94
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2023-12-19 17:17:07 +0200

    Add tkinter gui interface with bulkArchiver.py functionality

--- gui_launcher.py
+++ gui_launcher.py
@@ -1,21 +1,44 @@
 # Launcher for Graphical User Interface
 
+import threading
 import customtkinter as ctk
+from bulkArchiver import bulk_archiver
 
 
 class App(ctk.CTk):
 
     def __init__(self):
         super().__init__()
-        self.geometry('400x150')
+        self.geometry('800x600')
         self.title('File Management Toolkit')
 
-        self.button = ctk.CTkButton(self, text="my button", command=self.button_callback)
-        self.button.pack(padx=20, pady=20)
+        # Input for directory
+        self.directory_label = ctk.CTkLabel(self, text='Directory:')
+        self.directory_label.pack(padx=20, pady=10)
+        self.directory_entry = ctk.CTkEntry(self)
+        self.directory_entry.pack(padx=20, pady=10)
 
-    def button_callback(self):
-        print("button clicked")
+        # Input for excluded extensions
+        self.exclude_label = ctk.CTkLabel(self, text='Exclude Extensions (comma-separated):')
+        self.exclude_label.pack(padx=20, pady=10)
+        self.exclude_entry = ctk.CTkEntry(self)
+        self.exclude_entry.pack(padx=20, pady=10)
 
+        # Button to execute bulk_archiver
+        self.archive_button = ctk.CTkButton(self, text='Archive', command=self.archive_files)
+        self.archive_button.pack(padx=20, pady=10)
+
+    def archive_files(self):
+        directory = self.directory_entry.get()
+        exclude_str = self.exclude_entry.get()
+        exclude_extensions = set(exclude_str.split(',')) if exclude_str else None
+
+        threading.Thread(target=self.run_archiving, args=(directory, exclude_extensions)).start()
+
+    def run_archiving(self, directory, exclude_extensions):
+        contents, efficiency = bulk_archiver(directory, exclude_extensions)
+        print('Files in ZIP:', contents)
+        print('Compression Efficiency: {:.2f}'.format(efficiency))
 
 app = App()
 app.mainloop()
