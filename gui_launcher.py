8abde57395e5fc8b41317d1df1395d6f5206f771
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2024-01-08 16:13:25 +0200

    Adjust geometry

--- gui_launcher.py
+++ gui_launcher.py
@@ -16,7 +16,7 @@ from fileEncryptor import FileEncryptor
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry('800x600')
+        self.geometry('800x650')
         self.title('File Management Toolkit')
 
         # Side frame
